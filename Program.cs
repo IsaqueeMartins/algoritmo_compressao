759ae6bc8b9b1a7e8bf0f5937147fb051ac66a2f
Isaque Martins <isaque.martins.barros@gmail.com>
2025-03-09 13:26:53 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main()
+    {
+        OrdenaTexto();
+    }
+
+    static string OrdenaTexto()
+    {
+        Dictionary<Int32, String> listaPalavras = [];
+        int repeticoes = 0;
+        String linha;
+        String palavra;
+        try
+        {
+            StreamReader reader = new StreamReader("C:\\Users\\Isaque\\Desktop\\Arquivos\\arquivo_teste.txt");
+            linha = reader.ReadLine();
+            
+            while (linha != null)
+            {
+                listaPalavras.Add(repeticoes++, linha);
+                linha = reader.ReadLine();
+            }
+
+            reader.Close();
+            Console.WriteLine(String.Join(",", listaPalavras));
+        }
+        catch(Exception e)
+        {
+
+        }
+        finally
+        {
+
+        }
+
+        return "";
+    }
+}
